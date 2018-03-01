3b3172874254931aa0c59e8512b09fb94c29eee0
ZhekaPrice <36643027+ZhekaPrice@users.noreply.github.com>
2018-03-01 23:46:58 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+
+  var result = number;
+  var tempCount, tempNumber, tempbase = base, tempCount1;
+  for (var i = 2; i <= base ; i++)
+  {
+    tempNumber = number;
+    tempCount1 = 0;
+    if (tempbase % i == 0)
+    {
+      tempCount = 0;
+      while(tempbase % i == 0)
+      {
+        tempbase = Math.floor(tempbase/i);
+        tempCount++;
+      };
+      while(tempNumber/i > 0)
+      {
+        tempCount1 += Math.floor(tempNumber/i);
+        tempNumber = Math.floor(tempNumber/i);
+      };
+      result = Math.floor(Math.min(result,tempCount1/tempCount));
+    };
+  };
+  return result;
 }
